6d3276e891941f3c51d3311303e849a15f3c1a47
Nadia Disla <tejadanadiam@gmail.com>
2021-03-19 22:17:56 -0400

    Funcionamiento completo.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,67 @@
-window.addEventListener('load',function()
-{
+window.addEventListener('load', function () {
     var numbers = document.querySelectorAll(".numbers");
     var txtResult = document.querySelector("label");
-    var clear = document.querySelector("#clear");
+    var clearButton = document.querySelector(".row01");
+    var operations = document.querySelectorAll(".operations");
+    var num01, num02;
+    var control = 1;
+    var igual = document.querySelector("#igual");
 
-    for(var i = 0; i < numbers.length; i++)
-    {
-        numbers[i].addEventListener('click',function(){
-            if(txtResult.innerHTML == "0")
-            {
-                txtResult.innerHTML = "";
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', function () {
+            if (txtResult.innerHTML == 0 || control == -1) {
+                if (txtResult.innerHTML == 0) {
+                    txtResult.innerHTML = "";
+                }
+                if (control == -1) {
+                    txtResult.innerHTML = "";
+                    control = -2;
+                }
             }
             txtResult.innerHTML += this.textContent;
-        });    
+            if (control == 1) {
+                num01 = parseInt(txtResult.innerHTML);
+            }
+            else if (control < 0) {
+                num02 = parseInt(txtResult.innerHTML);
+            }
+        });
     }
-    clear.addEventListener('click', () => {txtResult.innerHTML = 0});
+
+    for (let i = 0; i < operations.length; i++) {
+        operations[i].addEventListener('click', function () {
+            op = this.textContent;
+            control = -1;
+        });
+    }
+
+    clearButton.addEventListener('click', () => { 
+        txtResult.innerHTML = 0;
+        num01 = 0;
+        num02 = 0;
+        control = 1;
+    });
+
+    igual.addEventListener('click', () => {
+        txtResult.innerHTML = "";
+        let resultado;
+        switch (op) {
+            case "+":
+                resultado = num01 + num02;
+                txtResult.innerHTML = resultado;
+                break;
+            case "-":
+                resultado = num01 - num02;
+                txtResult.innerHTML = resultado;
+                break;
+            case "x":
+                resultado = num01 * num02;
+                txtResult.innerHTML = resultado;
+                break;
+            case "÷":
+                resultado = num01 / num02;
+                txtResult.innerHTML = resultado;
+                break;
+        }
+    });
 });
